6b3cce557cf2e744dc60c19b048b447b07e30226
Chegarrito <ojedgar108@gmail.com>
2025-12-01 12:16:34 -0600

    Complete TODO's in UI.java

--- UI.java
+++ UI.java
@@ -5,22 +5,39 @@ public class UI
 	private String rMode;
 	
 	/* TODO add get and set methods for the runState */
-	
+	public boolean getRunState()
+	{
+		return runState;
+	}
+	public void setRunState(boolean state)
+	{
+		runState = state;
+	}
 	/* TODO add get method to return the holdTemp, hardcoded to 68 degrees */
-	
+	public int getHoldTemp()
+	{
+		return 68;
+	}
 	/* TODO add get method to return the rMode, initially hardcoded to "heat" for testing purposes */
-	
+	public String getRunMode()
+	{
+		return "heat";
+	}
 	public void printStatus(int currTemp, boolean commandMode)
 	{
-		print("Hold Temp: " + getHoldTemp() + " | Mode: " + getRunMode() + " | Furnace State: " + convertBool(getRunState()) + "| Current Temp: " + currTemp + " | Command Mode: " + convertBool(commandMode));
+		System.out.println("Hold Temp: " + getHoldTemp() + " | Mode: " + getRunMode() + " | Furnace State: " + convertBool(getRunState()) + "| Current Temp: " + currTemp + " | Command Mode: " + convertBool(commandMode));
 	}
 	
 	private String convertBool(boolean isOn)
 	{
 		/* TODO add a string variable to hold the on/off state as a string, initialized to "Off"  */
-		
+		String state = "Off";
 		/* TODO add an if statement, testing to see if isOn is true, and if it is, setting the string variable to "On" */
-		
+		if (isOn)
+		{
+			state = "On";
+		}
 		/* TODO add the return statement for the string */
+		return state;
 	}
 }
