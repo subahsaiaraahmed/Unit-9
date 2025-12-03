d7e5c22a64736e72a24b7315c8f570123c71a872
Chegarrito <ojedgar108@gmail.com>
2025-12-02 23:31:11 -0600

    AirConditioner TODO are done

--- AirConditioner.java
+++ AirConditioner.java
@@ -21,19 +21,22 @@ public class AirConditioner extends TimerTask
 			
 			/* TODO - translate the pseudocode logic below into working java code */
 			
-			if runState=on and the current temperature is more than 2 degrees under the hold temperature
-			{
-				set the command mode to off
+			//if runState=on and the current temperature is more than 2 degrees under the hold temperature
+			if(runState == true && currTemp < holdTemp - 2){
+				//set the command mode to off
 			}
-			else if runState=off and the current temperature is more than 2 degrees over the hold temperature
+			//else if runState=off and the current temperature is more than 2 degrees over the hold temperature
+			else if(runState == false && currTemp > holdTemp + 2)
 			{
-				set the command mode to on
+				//set the command mode to on
+				commandMode = true;
 			}
 			
 			
 			/* TODO - call the UI's print status method, passing to it the current temperature and command mode */
-			
+			myUI.printStatus(currTemp, commandMode);
 			/* TODO - call the UI's set run state method to pass the command mode to the UI */	
+			myUI.setRunState(commandMode);
 		}
 	
 	}
